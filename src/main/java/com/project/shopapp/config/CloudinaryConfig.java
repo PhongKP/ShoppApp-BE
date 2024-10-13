a344d6b2fe2624beb47ad569aabb5d0d70b12327
PhongKP <koongchanphong0712@gmail.com>
2024-10-13 16:07:28 +0700

    Secure api key

--- src/main/java/com/project/shopapp/config/CloudinaryConfig.java
+++ src/main/java/com/project/shopapp/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,21 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.api-key}")
+    private String api_key;
+
+    @Value("${cloudinary.api-secret}")
+    private String api_secret;
+
+    @Value("${cloudinary.name}")
+    private String cloudinaryName;
+
     @Bean
     public Cloudinary cloudinary(){
         Map config = new HashMap();
-        config.put("cloud_name", "dnziatudh");
-        config.put("api_key", "562738571532324");
-        config.put("api_secret", "FIfDXQOCnC6JFSoLQnxkzoW3K8E");
+        config.put("cloud_name", cloudinaryName);
+        config.put("api_key", api_key);
+        config.put("api_secret", api_secret);
         config.put("secure", true);
         return new Cloudinary(config);
     }
